d217ebc8f168523431e263bf27cdd5a34ae017f5
99hsueh <99hsueh@gmail.com>
2017-02-09 21:57:38 +0000

    sortByKey,use condition for a and b to apply next level sorting if a and b is equal

---
+++ sortByKey.js
@@ -0,0 +1,24 @@
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+
+//sort by ascending name(a - z),
+//if names are equal,
+// sort by descending age(old to young)
+
+function customSorting(a, b){
+//
+if(a.name > b.name){
+    return 1;
+  }
+  else {
+    if(a.age < b.age){
+      return 1;
+    }
+  }
+};
+
+console.log(students.sort(customSorting));
